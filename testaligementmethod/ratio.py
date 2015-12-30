c5a700d375898454dd7cfd973dde5669b5bfc315
郭一凡 <exonuclease57@gmail.com>
2015-12-30 16:31:29 +0800

    V1.0

---
+++ testaligementmethod/ratio.py
@@ -0,0 +1,36 @@
+﻿import glob
+from multiprocessing import Process
+import os.path
+def worker(fname):
+    areads = 0
+    reads = 0
+    file = open(fname)
+    for line in file:
+        areads = areads + int(line.split(',')[0].split('_')[1])
+    file.close()
+    fnames = glob.glob(r'/home/ldzeng/yfguo/jmzeng-summary/cluster/' + os.path.basename(fname)[0:8] + '*' + '.csv')
+    for fn in fnames:
+        file=open(fn)
+        for line in file:
+            reads = reads + int(line.split(',')[0].split('_')[1])
+        file.close()
+    filew = open(r'/home/ldzeng/yfguo/jmzeng-summary/cluster/MLS128/summary.csv','a')
+    filew.write(os.path.basename(fname) + ',' + str(areads) + ',' + str(reads) + ',' + str(areads / reads) + '\n')
+    filew.close()
+fnames = glob.glob(r'/home/ldzeng/yfguo/jmzeng-summary/cluster/MLS128/*MLS128aligement.csv')
+pool = []
+if __name__ == '__main__':
+    for fname in fnames:
+        if os.path.getsize(fname) > 0:
+            pool.append(Process(target=worker,args=(fname,)))
+            pool[-1].start()
+file=open(r'/home/ldzeng/yfguo/jmzeng-summary/cluster/MLS128/summary.csv')
+lines=[]
+for line in file:
+    lines.append(line)
+file.close()
+lines.sort(reverse=True,key=lambda x:float(x.strip().split(',')[-1]))
+filew=open(r'/home/ldzeng/yfguo/jmzeng-summary/cluster/MLS128/summary.csv','w')
+for line in lines:
+    filew.write(line)
+filew.close()
---
+++ testaligementmethod/testaligementmethod.py
@@ -0,0 +1,85 @@
+﻿from math import log2
+import glob
+from multiprocessing import Process
+def cpsfm(lines):
+    n = len(lines)
+    psfm = []
+    for i in range(0,len(lines[0])):
+        psfm.append({})
+        for line in lines:
+            if line[i] in psfm[i]:
+                psfm[i][line[i]] = psfm[i][line[i]] + 1 / n
+            else:
+                psfm[i][line[i]] = 1 / n
+        for i in range(0,len(psfm)):
+            se = 0
+            for feq in psfm[i].values():
+                se = se - feq * log2(feq)
+            if se > 1.7:
+                psfm[i] = {}
+    return psfm
+def cprob(seqs,psfm):
+    probs = []
+    for i in range(0,len(seqs)):
+        prob = 1
+        for j in range(0,len(seqs[i])):
+            if psfm[j]:
+                if seqs[i][j] in psfm[j]:
+                    prob = prob * psfm[j][seqs[i][j]]
+                else:
+                    prob = prob * 0.01
+        probs.append(prob)
+    return probs
+def worker(fname):
+    file = open(r'/home/ldzeng/yfguo/jmzeng-summary/cluster/MLS128/trainset.txt')
+    trainset = []
+    for line in file:
+        for i in range(0,2):
+            trainset.append(line.strip())
+    file.close()
+    file = open(fname)
+    lines = []
+    seqs = []
+    for line in file:
+        if line.split(',')[-1] == '98\n':
+            lines.append(line.strip())
+    file.close()
+    for line in lines:
+        seqs.append(line.split(',')[-4])
+    if 0.0001 * len(seqs) < 1:
+        tsize = 0
+    else:
+        tsize = int(0.0001 * len(seqs))
+    for i in range(0,5):
+        psfm = cpsfm(trainset)
+        probs = cprob(seqs,psfm)
+        for i in range(0,tsize):
+            trainset.append(seqs.pop(probs.index(max(probs))))
+            del probs[probs.index(max(probs))]
+    seqs = []
+    for line in lines:
+        seqs.append(line.split(',')[-4])
+    probs = cprob(seqs,psfm)
+    file = open(r'/home/ldzeng/yfguo/jmzeng-summary/cluster/MLS128/trainset.txt')
+    ptrainset = []
+    for line in file:
+        ptrainset.append(line.strip())
+    tprobs = cprob(ptrainset,psfm)
+    cutoff = min(tprobs)
+    file.close()
+    filew = open(fname[0:-4] + 'aligementall.csv','w')
+    for i in range(0,len(lines)):
+        if probs[i] > 0:
+            filew.write(lines[i] + ',' + str(probs[i]) + '\n')
+    filew.close()
+    filew = open(fname[0:-4] + 'aligement.csv','w')
+    for i in range(0,len(lines)):
+        if probs[i] > cutoff:
+            filew.write(lines[i] + ',' + str(probs[i]) + '\n')
+    filew.close()
+fnames = glob.glob(r'/home/ldzeng/yfguo/jmzeng-summary/cluster/MLS128/*MLS128.csv')
+pool = []
+if __name__ == '__main__':
+    for fname in fnames:
+        pool.append(Process(target=worker,args=(fname,)))
+        pool[-1].start()
